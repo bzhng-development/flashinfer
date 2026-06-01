c02dd7735fd722878e1ec66dee9cf420feef016b
Brayden Zhong <brayden.zhong@radixark.ai>
2026-06-01 22:05:51 +0000

    more

--- csrc/group_gemm_nvfp4_groupwise_sm120.cu
+++ csrc/group_gemm_nvfp4_groupwise_sm120.cu
@@ -43,6 +43,9 @@ using namespace flashinfer;
     } else if (tile_n == 16) {                     \
       constexpr int TILE_N = 16;                   \
       return __VA_ARGS__();                        \
+    } else if (tile_n == 8) {                      \
+      constexpr int TILE_N = 8;                    \
+      return __VA_ARGS__();                        \
     }                                              \
     TVM_FFI_ICHECK(false) << "Unsupported TILE N"; \
     return false;                                  \
